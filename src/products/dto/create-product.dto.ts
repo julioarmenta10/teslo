b01f94ef364ea5b06a34bc6d44d4126ca7e4f541
JulioArmenta <julioarmentaher@gmail.com>
2023-01-19 19:28:18 -0700

    Segundo commit hasta la seccion 10 terminada

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,39 @@
+import { IsString, IsIn, IsArray, IsInt, IsNumber, IsOptional, IsPositive, MinLength } from "class-validator";
+
+export class CreateProductDto {
+
+    @IsString()
+    @MinLength(1)
+    title: string;
+
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    price?: number;
+
+    @IsString()
+    @IsOptional()
+    description?: string;
+
+    @IsString()
+    @IsOptional()
+    slug?: string;
+
+    @IsInt()
+    @IsPositive()
+    @IsOptional()
+    stock?: number;
+
+    @IsString({ each: true })
+    @IsArray()
+    sizes: string[]
+
+    @IsIn(['men', 'woman', 'kid', 'unisex'])
+    gender: string;
+
+
+    @IsString({ each: true })
+    @IsArray()
+    @IsOptional()
+    tags: string[]
+}
